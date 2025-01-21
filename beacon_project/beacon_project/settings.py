c802e26a8785d1b6fd91762d99392e780d86e2e2
P1R47E <yosephtesfaye27@gmail.com>
2025-01-21 14:58:04 +0300

    final

--- beacon_project/beacon_project/settings.py
+++ beacon_project/beacon_project/settings.py
@@ -90,6 +90,27 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
 ]
 
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 # REST Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
